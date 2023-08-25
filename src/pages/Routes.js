5d83bf3164e9aa424989db657390c91f5a566903
Abdullah Murtaza <99031293+abdiopp@users.noreply.github.com>
2023-08-26 04:18:34 +0500

    fixed

--- src/pages/Routes.js
+++ src/pages/Routes.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
-
 export default function CustomRoutes() {
   return (
     <>
